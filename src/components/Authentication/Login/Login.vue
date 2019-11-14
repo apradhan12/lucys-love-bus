444eec22c8c014f21fbdef0330c76e8aa8ca9086
Nick Burniske <nicoburniske@gmail.com>
2019-11-14 13:50:51 -0500

    implemented wireframe login and signup components.
    This has full feature validation and async login action compatible. Store is not setup properly yet.
    This implemenation doesn't leverage slots. Second draft will be completed soon.

---
+++ src/components/Authentication/Login/Login.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <slot name="login-header"> default header </slot>
+    <slot name="login-description"> default description </slot>
+    <div class="login-container">
+      <slot name="login-prompt"> default prompt </slot>
+      <input v-model="email" @focus="resetSubmit" type="text" placeholder="Email Address" />
+      <input v-model="password" @focus="resetSubmit" type="password" placeholder="Password"/>
+      <button @click="submit"> Login </button>
+      <router-link to="sign-up" tag="a"> Don't have an account? Sign up here! </router-link>
+    </div>
+    <div class="error">
+      <p v-if="!inputValid() && submitted"> Enter an email and password </p>
+      <p v-if="error && submitted">{{error}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      email: '',
+      password: '',
+      submitted: false,
+      error: '',
+    };
+  },
+  methods: {
+    resetInput() {
+      this.email = '';
+      this.password = '';
+      this.resetSubmit();
+    },
+    resetSubmit() {
+      this.submitted = false;
+      this.error = '';
+    },
+    inputValid() {
+      return this.email && this.password;
+    },
+    async submit() {
+      this.submitted = true;
+      if (this.inputValid()) {
+        const user = { email: this.email, password: this.password };
+        try {
+          await this.$store.dispatch('login', user);
+          this.resetInput();
+          this.$router.push('/home');
+        } catch (error) {
+          this.error = `Incorrect Email/Password: ${error.message}`;
+        }
+      }
+    },
+  },
+};
+</script>
+
+
+<style lang="less" scoped>
+  .login-container {
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    button {
+      width: 20%;
+    }
+  }
+</style>
---
+++ src/components/Authentication/SignUp/SignUp.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <h1> Signup </h1>
+    <slot name="sign-up-header"> default header </slot>
+    <slot name="sign-up-description"> default description </slot>
+    <div class="sign-up-container">
+      <slot name="sign-up-prompt"> default prompt </slot>
+      <input v-model="name[0]" type="text" placeholder="First Name">
+      <input v-model="name[1]" type="text" placeholder="Last Name">
+      <input v-model="email" type="text" placeholder="Email">
+      <input v-model="password[0]" type="password" placeholder="Password">
+      <input v-model="password[1]" type="password" placeholder="Retype Password">
+      <button @click="signup"> Signup </button>
+    </div>
+    <div class="error">
+      <p v-show="!!inputError[0]"> {{inputError[0]}} </p>
+      <p v-show="!!inputError[1]"> {{inputError[1]}} </p>
+      <p v-show="!!inputError[2]"> {{inputError[2]}} </p>
+      <p v-if="this.serverError"> {{this.serverError}} </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignupForm',
+  data() {
+    return {
+      name: ['', ''],
+      email: '',
+      password: ['', ''],
+      inputError: ['', '', ''],
+      serverError: '',
+    };
+  },
+  methods: {
+    resetInput() {
+      this.name = ['', ''];
+      this.email = '';
+      this.password = ['', ''];
+      this.inputError = ['', '', ''];
+      this.serverError = '';
+    },
+    validate() {
+      return this.validateUser() && this.validateEmail() && this.validatePassword();
+    },
+    validateUser() {
+      if (!this.name[0] || !this.name[1]) {
+        this.inputError.splice(0, 1, 'Name cannot be empty');
+      } else {
+        this.inputError.splice(0, 1, '');
+        return true;
+      }
+      return false;
+    },
+    validateEmail() {
+      // eslint-disable-next-line no-useless-escape
+      const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!this.email) {
+        this.inputError.splice(1, 1, 'Email cannot be empty');
+      } else if (!emailRegex.test(this.email)) {
+        this.inputError.splice(1, 1, 'Invalid Email');
+      } else {
+        this.inputError.splice(1, 1, '');
+        return true;
+      }
+      return false;
+    },
+    validatePassword() {
+      if (!this.password[0] || !this.password[1]) {
+        this.inputError.splice(2, 1, 'Password cannot be empty');
+      } else if (this.password[0] !== this.password[1]) {
+        this.inputError.splice(2, 1, 'Passwords do not match');
+      } else {
+        this.inputError.splice(2, 1, '');
+        return true;
+      }
+      return false;
+    },
+    async signup() {
+      this.submitted = true;
+      this.serverError = '';
+      if (this.validate()) {
+        const user = {
+          firstName: this.name[0],
+          lastName: this.name[1],
+          email: this.email,
+          password: this.password[0],
+        };
+        try {
+          await this.$store.dispatch('signup', user);
+          this.resetInput();
+          this.$router.push('/home');
+        } catch (error) {
+          this.serverError = error.message;
+        }
+      }
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+  .sign-up-container {
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    button {
+      width: 20%;
+    }
+  }
+</style>
