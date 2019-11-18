a11be9b873ec9114633a8ae5aca2e0f7bbdb64f4
ZachOBrien <zachobrien99@gmail.com>
2019-11-17 22:13:14 -0500

    cleaned up the events list components

--- src/components/Events/Events.vue
+++ src/components/Events/Events.vue
@@ -1,22 +1,81 @@
 <template>
     <div>
-        <h1>Our Upcoming Events</h1>
-        <TheEventsList />
+        <div class="events-list__container">
+          <h1>Our Upcoming Events</h1>
+          <div class='pagination-wrapper'>
+            <button
+              class='pagination__btn'
+              v-on:click='decrementCurrentPage'>decrement</button>
+
+            <h4>Week</h4>
+            <button
+              class='pagination__btn'
+              v-on:click='incrementCurrentPage'>increment</button>
+          </div>
+          <Event
+              v-for='event in pageOfEvents'
+              :key='event.id'
+              :id='event.id'
+              :name='event.name'
+              :description='event.description' />
+        </div>
     </div>
 </template>
 
 <script>
-import TheEventsList from './TheEventsList.vue';
+import Event from './Event.vue';
 
 export default {
   name: 'Events',
   components: {
-    TheEventsList,
+    Event,
+  },
+  data() {
+    return {
+      eventsPerPage: 5,
+      currentPage: 0,
+    };
+  },
+  computed: {
+    // list of a single page's worth of items
+    pageOfEvents() {
+      return this.$store.getters.getPageOfEvents(this.currentPage, this.eventsPerPage);
+    },
+    // page number of the last page
+    maxPage() {
+      return Math.floor(this.$store.getters.numOfEvents / this.eventsPerPage);
+    },
+  },
+  methods: {
+    incrementCurrentPage() {
+      if (this.currentPage < this.maxPage) {
+        this.currentPage += 1;
+      }
+    },
+    decrementCurrentPage() {
+      if (this.currentPage > 0) {
+        this.currentPage -= 1;
+      }
+    },
   },
 };
 
 </script>
 
 <style>
+  .events-list__container {
+    margin: 0 auto;
+    width: 65rem;
+    max-width: 90vw;
+  }
 
+  .pagination-wrapper {
+    display: flex;
+    justify-content: center;
+  }
+
+  .pagination__btn {
+    margin-left: 1rem;
+    margin-right: 1rem;
+  }
 </style>
