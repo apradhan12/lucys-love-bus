2ebd73b4a6934904fedd79eac083c96ebef424a9
ZachOBrien <zachobrien99@gmail.com>
2020-03-06 22:46:41 -0500

    layout complete

---
+++ src/components/Checkout/Checkout.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h1>My Cart</h1>
+    <h3>You signed up for the following events:</h3>
+    <div class="component-container">
+      <div class="events"></div>
+      <div class="payment"></div>
+      <div class="codes"></div>
+    </div>
+    <the-footer />
+  </div>
+</template>
+
+<script>
+import TheFooter from '../TheFooter.vue';
+
+export default {
+  name: 'Checkout',
+  components: {
+    TheFooter,
+  },
+};
+</script>
+
+<style>
+  .component-container {
+    margin: 0 auto;
+    padding: 1rem;
+    display: grid;
+    grid-template-areas: 'events payment'
+                         'events codes'
+                         'events _';
+    grid-template-columns: 2fr 1fr;
+    grid-template-rows: 2fr 1fr 1fr;
+    grid-gap: 1rem;
+    width: 50vw;
+    height: 40rem;
+    border: 1px solid red;
+  }
+
+  .events {
+    grid-area: events;
+    background-color: lightgreen;
+  }
+
+  .payment {
+    grid-area: payment;
+    background-color: lightcoral;
+  }
+
+  .codes {
+    grid-area: codes;
+    background-color: lightseagreen;
+  }
+</style>
