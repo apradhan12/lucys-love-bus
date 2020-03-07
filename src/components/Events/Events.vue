413c729bcebcaec1580fc67f354b3a689d678b71
ZachOBrien <zachobrien99@gmail.com>
2020-03-06 23:12:30 -0500

    scrolling events list complete

--- src/components/Events/Events.vue
+++ src/components/Events/Events.vue
@@ -1,21 +1,36 @@
 <template>
   <div>
     <h1>Events</h1>
-    <EventsList :events="events"></EventsList>
+    <events-list v-if="isPaginate" :events="events"></events-list>
+    <EventsListScroll v-if="isScroll" :events="events"></EventsListScroll>
   </div>
 </template>
 
 <script>
 import EventsList from './EventsList.vue';
+import EventsListScroll from './EventsListScroll.vue';
 
 export default {
   name: 'Events',
   components: {
     EventsList,
+    EventsListScroll,
+  },
+  props: {
+    style: { type: String, default: 'scroll' },
   },
   computed: {
     events() {
       return this.$store.getters['events/getEvents'];
+    },
+    isPaginate() {
+      return this.style === 'paginate';
+    },
+    isScroll() {
+      return this.style === 'scroll';
+    },
+    validStyle() {
+      return (this.style === 'paginate' || this.style === 'scroll');
     },
   },
 };
