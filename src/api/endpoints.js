5262eb1bb9a9482ebd35e0b3df6909784d7dbad4
Nick Burniske <nicoburniske@gmail.com>
2020-03-29 11:16:47 -0400

    fixed llb backend url

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,5 +1,5 @@
 // API urls
-export const API_DOMAIN = 'http://lucy.c4cneu.edu/';
+export const API_DOMAIN = 'http://lucy.c4cneu.com/';
 export const API_LOGIN = 'api/v1/user/login/';
 export const API_REFRESH_TOKEN = 'api/v1/user/login/refresh/';
 export const API_SIGNUP = 'api/v1/user/signup/';
