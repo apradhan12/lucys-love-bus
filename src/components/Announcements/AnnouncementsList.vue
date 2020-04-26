70e05514a1b5c88331874cd1e1c3339f65218ef4
Zach O'Brien <zachobrien99@gmail.com>
2020-04-26 13:32:23 -0400

    announcements list complete

--- src/components/Announcements/AnnouncementsList.vue
+++ src/components/Announcements/AnnouncementsList.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="scroll-container">
-    <div v-for="a in allAnnouncements" v-bind:key="a.id">
-      <h3>{{a.title}}</h3>
-      <p>{{a.description}}</p>
-    </div>
+  <div class="scroll-container" v-bind:style="{ 'maxHeight': '15rem' }">
+      <div v-for="a in allAnnouncements" v-bind:key="a.id">
+        <h3>{{a.title}}</h3>
+        <p>{{a.description}}</p>
+      </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapGetters } from 'vuex';
 
 export default {
   name: 'AnnouncementsList',
   computed: {
-    ...mapState('announcements', {
+    ...mapGetters('announcements', {
       allAnnouncements: 'getAllAnnouncements',
     }),
   },
