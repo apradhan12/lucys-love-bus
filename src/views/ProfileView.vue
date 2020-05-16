82db53d86a21e8c02d5e6b6d5ae97b4ded247780
Liam Moynihan <liammoyn@gmail.com>
2020-05-16 14:48:15 -0400

    Stylistic changes for announcements and implemented announcement modal

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -6,7 +6,9 @@
     <div class="flex-horizontal">
       <div class="announcements-list__container">
         <h4 class="dekko-label">Announcements</h4>
-        <announcements-list sitewide :count="announcementsCount"/>
+        <announcements-list sitewide
+                            :count="announcementsCount"
+                            @on-open-announcement="openAnnouncementModal"/>
       </div>
       <div class='flex-vertical'>
         <h4 class="dekko-label">Explore</h4>
@@ -27,6 +29,9 @@
         </router-link>
       </div>
       </div>
+      <AnnouncementModal :open="openModal"
+                         :announcement="modalAnnouncement"
+                         @on-close-announcement="closeAnnouncementModal"/>
     </div>
   </div>
 </template>
@@ -34,6 +39,7 @@
 <script>
 import Welcome from '../components/Profile/Welcome.vue';
 import AnnouncementsList from '../components/Announcements/AnnouncementsList.vue';
+import AnnouncementModal from '../components/Announcements/AnnouncementModal.vue';
 import ANNOUNCEMENT_COUNT from '../utils/constants/announcements';
 import { USER, ROLE } from '../utils/constants/user';
 import AccessControl from '../components/AccessControl/AccessControl.vue';
@@ -44,16 +50,28 @@ export default {
     return {
       USER,
       ROLE,
+      openModal: false,
+      modalAnnouncement: null,
     };
   },
   components: {
     Welcome,
     AnnouncementsList,
+    AnnouncementModal,
     AccessControl,
   },
   computed: {
     announcementsCount() {
       return ANNOUNCEMENT_COUNT;
+    },
+  },
+  methods: {
+    openAnnouncementModal(announcement) {
+      this.openModal = true;
+      this.modalAnnouncement = announcement;
+    },
+    closeAnnouncementModal() {
+      this.openModal = false;
     },
   },
 };
